9ea34d6411c7200247680fbca6ed03da75bc745e
ivangalvan <ivan.galvan.femenia@gmail.com>
2019-02-02 21:07:50 +0100

    Update example.R

--- example.R
+++ example.R
@@ -3,7 +3,8 @@ setwd("github")
 source("logratio_biplot_pca.R")
 
 
-# logratio biplot with "UN","6TH","5TH","4TH","3RD","2ND","3.4S","FS" relationships and peel and zoom approach
+# logratio biplot with "UN","6TH","5TH","4TH","3RD","2ND","3.4S","FS" relationships 
+# and peel and zoom approach
 
 my_logpca = logratio_biplot_pca(data.in="data/CEU",
                                 data.out = "CEU_population",
@@ -17,7 +18,7 @@ table(my_logpca$pairs.ids$prediction)
 
 my_logpca = logratio_biplot_pca(data.in="data/CEU",
                                 data.out = "CEU_population",
-                                nsim.rel = 5,
+                                nsim.rel = 20,
                                 peel.and.zoom = F)
 
 
@@ -26,7 +27,7 @@ my_logpca = logratio_biplot_pca(data.in="data/CEU",
 
 my_logpca = logratio_biplot_pca(data.in="data/CEU",
                                 data.out = "CEU_population",
-                                nsim.rel = 5,
+                                nsim.rel = 20,
                                 peel.and.zoom = F,
                                 relationships = c("UN","4TH","3RD","2ND","3.4S","FS"),
                                 rel.colors = c("forestgreen","cyan2",
