76e3c50e05bce694046d563d09dd1addd38a2c8a
ivangalvan <ivan.galvan.femenia@gmail.com>
2019-02-09 13:06:02 +0100

    Update example.R

--- example.R
+++ example.R
@@ -1,22 +1,22 @@
 setwd("github")
 
-source("kinship_biplot.R")
+source("LR_kinbiplot.R")
 
 
 # logratio biplot with "UN","6TH","5TH","4TH","3RD","2ND","3.4S","FS" relationships 
 # and peel and zoom approach
 
-my_logpca = kinship_biplot(data.in="data/CEU",
+logpca = LR_kinbiplot(data.in="data/CEU",
                                 data.out = "CEU_population",
                                 nsim.rel = 20,
                                 peel.and.zoom = T)
 
-table(my_logpca$pairs.ids$prediction)
+table(logpca$pairs.ids$prediction)
 
 
 # logratio biplot with "UN","6TH","5TH","4TH","3RD","2ND","3.4S","FS" relationships 
 
-my_logpca = kinship_biplot(data.in="data/CEU",
+logpca = LR_kinbiplot(data.in="data/CEU",
                                 data.out = "CEU_population",
                                 nsim.rel = 20,
                                 peel.and.zoom = F)
@@ -25,7 +25,7 @@ my_logpca = kinship_biplot(data.in="data/CEU",
 # logratio biplot with "UN","4TH","3RD","2ND","3.4S","FS" relationships 
 
 
-my_logpca = kinship_biplot(data.in="data/CEU",
+logpca = LR_kinbiplot(data.in="data/CEU",
                                 data.out = "CEU_population",
                                 nsim.rel = 20,
                                 peel.and.zoom = F,
